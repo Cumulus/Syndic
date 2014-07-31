b63af58831a65139a9f1ebc740a8f860f7e308e5
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-31 23:37:57 +0200

    Configure travis to build and publish documentation

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,3 +9,23 @@ eval `opam config env`
 
 ./configure
 make
+
+
+if [ "$TRAVIS_REPO_SLUG" == "Cumulus/Syndic" ] \
+     && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
+     && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing ocamldoc...\n"
+
+  make doc
+
+  git config --global user.email "romain.calascibetta@gmail.com"
+  git config --global user.name "dinosaure"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Cumulus/Syndic gh-pages
+
+  git add -f doc/
+  git commit -m "Lastest ocamldoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages
+
+  echo -e "Published ocamldoc to gh-pages.\n"
+fi
