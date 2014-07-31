2bf4daad5a562e86b35836465796b4c4da14f762
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-31 22:45:28 +0200

    An binary test

---
+++ bin/main.ml
@@ -0,0 +1,5 @@
+let () =
+  try let _ = Atom.analyze (Xmlm.make_input (`Channel stdin)) in ()
+  with
+    | Atom.Error.Expected (a, b) -> print_endline (Atom.Error.string_of_expectation (a, b))
+    | _ -> print_endline "Unknown error"
