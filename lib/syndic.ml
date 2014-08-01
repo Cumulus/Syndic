04c18319147c0e520fd13cbdf61369e4d41d4046
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-08-01 11:27:16 +0200

    Pack all modules in Syndic

---
+++ lib/syndic.ml
@@ -0,0 +1,3 @@
+module Rss1 = Rss1
+module Rss2 = Rss2
+module Atom = Atom
--- lib/syndic.mli
+++ lib/syndic.mli
@@ -1,1 +1,5 @@
 (** Syndic module *)
+
+module Rss1 : (module type of Rss1)
+module Rss2 : (module type of Rss2)
+module Atom : (module type of Atom)
