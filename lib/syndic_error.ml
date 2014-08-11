8ae25f8cc67777882f831bb75503c936d4565294
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-08-11 15:22:17 +0200

    Remove useless exception

--- lib/syndic_error.ml
+++ lib/syndic_error.ml
@@ -1,32 +1,12 @@
 open Printf
 
-type expected =
-  | Attr of string
-  | Tag of string
-  | Data
-  | Root
-
 type error = [
   | `Expected of string
 ]
 
-exception Expected of expected * expected
-exception Expected_Data
 exception Error of Xmlm.pos * error
-
-let string_of_expected () = function
-  | Attr a -> a ^ "="
-  | Tag a -> "<" ^ a ^ ">"
-  | Data -> "data"
-  | Root -> "root"
-
-let string_of_expectation (a, b) =
-  sprintf "Expected %a in %a" string_of_expected a string_of_expected b
 
 let to_string = function
   | Error (pos, `Expected str) ->
     sprintf "%s at l.%d" str (fst pos)
   | exn -> Printexc.to_string exn
-
-let raise_expectation data in_data = raise (Expected (data, in_data))
-
--- lib/syndic_error.mli
+++ lib/syndic_error.mli
@@ -1,20 +1,10 @@
 
 (** The common signature that all error modules must (at least) satisfy. *)
 
-type expected =
-  | Attr of string
-  | Tag of string
-  | Data
-  | Root
-
 type error = [
   | `Expected of string
 ]
 
-exception Expected of expected * expected
-exception Expected_Data
 exception Error of Xmlm.pos * error
 
 val to_string : exn -> string
-val string_of_expectation : expected * expected -> string
-val raise_expectation : expected -> expected -> 'a
