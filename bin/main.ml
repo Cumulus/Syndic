209112cab51bdc237069f033e36cfa86b69daacf
Eyyub <eyyub.pangearaion@gmail.com>
2014-10-19 14:53:17 +0200

    Commit Syndic.Opml1

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,47 @@
-open Syndic
+open Syndic.Opml1
+open Printf
+
+(* let () = *)
+
+(*   Printexc.record_backtrace true; *)
+
+(*   try let _ = Atom.parse (Xmlm.make_input (`Channel stdin)) in () *)
+(*   with *)
+(*     | Atom.Error.Error (pos, err) -> *)
+(*         print_endline err *)
+(*     | _ -> Printexc.print_backtrace stderr *)
+
+let print_head h =
+  printf "Title : %s\n" h.title;
+  printf "Date Creation : %f\n" (CalendarLib.Calendar.to_unixfloat h.date_created);
+  printf "Date Modified : %f\n" (CalendarLib.Calendar.to_unixfloat h.date_modified);
+  printf "Name %s\n" h.owner_name;
+  printf "Email %s\n" h.owner_email;
+  printf "Expansion State ["; List.iter (printf "%d,") h.expansion_state; printf "]\n";
+  printf "Window Top %d\n" h.window_top;
+  printf "Window Left %d\n" h.window_left;
+  printf "Window Bottom %d\n" h.window_bottom;
+  printf "Window Right %d\n" h.window_right
+
+let rec print_outline level o =
+  printf "%s Text %s\n" level (match o.text with Some s -> s | None -> "<None>");
+  List.iter (print_outline "Sub") o.outlines
+
+let print_body b =
+  List.iter (print_outline "High") b
+
+let print_opml1 o =
+  printf "Version : %s\n" o.version;
+  print_head o.head;
+  print_body o.body
 
 let () =
 
   Printexc.record_backtrace true;
 
-  try let _ = Atom.parse (Xmlm.make_input (`Channel stdin)) in ()
+  try let o = parse (Xmlm.make_input (`Channel stdin)) in print_opml1 o
   with
-    | Atom.Error.Error (pos, err) ->
+  | Error.Error (pos, err) ->
         print_endline err
-    | _ -> Printexc.print_backtrace stderr
+  | _ -> Printexc.print_backtrace stderr
+				  
--- lib/syndic.ml
+++ lib/syndic.ml
@@ -2,5 +2,6 @@
 module Rss1 = Syndic_rss1
 module Rss2 = Syndic_rss2
 module Atom = Syndic_atom
+module Opml1 = Syndic_opml1
 
 module XML = Syndic_xml
