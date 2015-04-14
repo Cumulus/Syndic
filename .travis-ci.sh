2075b63393fc05a37506faff2475afbbacfaa00b
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-04-14 20:20:56 +0200

    Fix Travis CI

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,10 @@
-echo "yes" | sudo add-apt-repository ppa:avsm/ppa
-sudo apt-get update -qq
-sudo apt-get install ocaml ocaml-native-compilers camlp4-extra opam opam
+sh .travis-ocaml.sh
 
-export OPAMYES=1
-opam init
-opam update
-opam install oasis ocamlfind calendar xmlm uri
-eval `opam config env`
+depends={DEPENDS:-""}
+
+# Install the external dependencies
+echo "opam install ${depends}"
+opam install ${depends}
 
 ./configure
 make
