d05f9287daecb0a4824f98c73900a9154961d8bc
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-05-26 11:46:31 +0200

    Add test used to motivate some design decisions

---
+++ test/test_decisions.ml
@@ -0,0 +1,29 @@
+(* Use the W3C validator to motivate some decisions that were taken. *)
+
+open Printf
+open Lwt
+open Syndic
+open Cohttp
+module Client = Cohttp_lwt_unix.Client
+module Body = Cohttp_lwt_body
+
+let check_feed data =
+  printf "ATOM: %s\n" data;
+  Client.get (W3C.url (`Data data)) >>= fun (r, b) ->
+  Body.to_string b >>= fun body ->
+  let (err, warn) = `String(0, body) |> Xmlm.make_input |> W3C.parse in
+  List.iter (fun e -> printf "ERROR: %s\n" e.W3C.text) err;
+  List.iter (fun e -> printf "WARNING: %s\n" e.W3C.text) warn;
+  return()
+
+let empty_title =
+  "<?xml version='1.0' encoding='utf-8'?>
+   <feed xmlns='http://www.w3.org/2005/Atom'>
+     <title></title>
+   </feed>"
+
+let main =
+  check_feed empty_title
+
+let () =
+  Lwt_main.run main
