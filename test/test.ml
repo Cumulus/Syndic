592a2bbbaac21ac65596cdc1a11e080f9aa266bf
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-11-08 07:37:36 -0800

    Update test and Travis CI (use now, travisci-skeleton)

--- test/test.ml
+++ test/test.ml
@@ -21,9 +21,17 @@ type fmt =
   | `Rss2
   | `Opml1 ]
 
+let timer =
+  let already_used = ref false in
+  (fun () ->
+    if !already_used
+    then Lwt_unix.sleep 1.0 >>= fun () -> already_used := true; Lwt.return ()
+    else (already_used := true; Lwt.return ()))
+
 let get : src -> Xmlm.source Lwt.t = function
   | `Uri src ->
-     CLU.Client.get src
+     timer ()
+     >>= fun () -> CLU.Client.get src
      >>= fun (response, body) -> CLB.to_string body
      >>= fun data -> Lwt.return (`String (0, data))
   | `Data data -> Lwt.return (`String (0, data))
@@ -39,7 +47,7 @@ let string_of_src = function
   | `Data data ->
      let buffer = Buffer.create 16 in
      Buffer.add_string buffer (String.sub data 0 16);
-     Buffer.add_string buffer "...";
+     Buffer.add_string buffer "…";
      Buffer.contents buffer
 
 let string_of_fmt = function
@@ -51,6 +59,12 @@ let string_of_fmt = function
 let tests : ([> src ] * [< fmt ] * result) list =
   [
     (`Uri (Uri.of_string "http://ocaml.org/feed.xml"), `Atom, Ok);
+    (`Uri (Uri.of_string "http://korben.info/feed"), `Rss2, Ok);
+    (`Uri (Uri.of_string "http://linuxfr.org/journaux.atom"), `Atom, Ok);
+    (`Uri (Uri.of_string "https://www.reddit.com/r/ocaml/.rss"), `Rss2, Ok);
+    (`Data ("<?xml version='1.0' encoding='utf-8'?> \
+             <feed xmlns='http://www.w3.org/2015/Atom'> \
+             <title></title></feed>"), `Atom, W3CError []);
   ]
 
 let () =
@@ -70,7 +84,6 @@ module Printf =
         | x :: r ->
           Printf.fprintf ch "%a%s" add_data x sep; aux r
       in aux lst
-
   end
 
 let state, switch =
@@ -78,10 +91,104 @@ let state, switch =
   (fun () -> !e),
   (fun () -> e := true)
 
+let sp = Printf.sprintf
+
+let red     fmt = sp ("\027[31m" ^^ fmt ^^ "\027[m")
+let green   fmt = sp ("\027[32m" ^^ fmt ^^ "\027[m")
+let yellow  fmt = sp ("\027[33m" ^^ fmt ^^ "\027[m")
+let blue    fmt = sp ("\027[36m" ^^ fmt ^^ "\027[m")
+let gray    fmt = sp ("\027[37m" ^^ fmt ^^ "\027[m")
+let red_s       = red "%s"
+let green_s     = green "%s"
+let yellow_s    = yellow "%s"
+let blue_s      = blue "%s"
+let gray_s      = gray "%s"
+
+let with_process_in cmd f =
+  let ic = Unix.open_process_in cmd in
+  try
+    let r = f ic in
+    ignore (Unix.close_process_in ic); r
+  with exn ->
+    ignore (Unix.close_process_in ic); raise exn
+
+let columns =
+  try
+    with_process_in "tput cols" (fun ic -> int_of_string (input_line ic))
+  with _ -> try
+    with_process_in "stty size" (fun ic ->
+      match Stringext.split (input_line ic) ~on:' ' with
+      | [_; v] -> int_of_string v
+      | _ -> failwith "stty")
+  with _ -> try int_of_string (Sys.getenv "COLUMNS")
+  with _ -> 80
+
+let line oc ?color c =
+  let line = match color with
+    | Some `Blue   -> blue_s (String.make columns c)
+    | Some `Yellow -> yellow_s (String.make columns c)
+    | None         -> String.make columns c
+  in Printf.fprintf oc "%s\n%!" line
+
+let left s n =
+  let n = n - String.length s in
+  if n <= 0 then s
+  else s ^ (String.make n ' ')
+
+let left_columns = 20
+
+let print s = Printf.printf "%s%!" s
+
+let print_info (src, fmt) =
+  print (sp "%s %s" (blue_s (string_of_fmt fmt)) (string_of_src src))
+
+let error (src, fmt) str =
+  print (left (red "[ERROR]") left_columns);
+  print_info (src, fmt);
+  Printf.printf str
+
+let expected (src, fmt) str =
+  print (left (green "[EXPECTED]") left_columns);
+  print_info (src, fmt);
+  Printf.printf str
+
+let result_equal a b = match a, b with
+  | SyndicError (_, err),
+    SyndicError (_, err') -> err = err'
+  | W3CError _, W3CError _ -> true
+  | _, _ -> false
+
+let print_result p (r, e) =
+  let add_error _ (pos, err) =
+    print (left (gray_s "W3C:") left_columns);
+    print (Syndic_error.Error (pos, err)
+           |> Syndic_error.to_string)
+  in
+  match r with
+  | SyndicError (pos, err) when result_equal r e ->
+    (expected p "\nsyndic: %s") err
+  | SyndicError (pos, err) ->
+    (error p "\nsyndic: %s") err
+  | W3CError errors when result_equal r e ->
+    (expected p "\n%a")
+      (Printf.add_list ~sep:"\n" add_error)
+      errors
+  | W3CError errors ->
+    (error p "\n%a")
+      (Printf.add_list ~sep:"\n" add_error)
+      errors
+  | AnotherError exn ->
+    (error p "exn: %s") (Printexc.to_string exn)
+  | Ok ->
+    print (left (green "[OK]") left_columns);
+    print_info p
+
+let newline () = print "\n"
+let reset () = print "\r"
+
 let make_test (src, fmt, result) =
-  Printf.printf "-+-+-+-+-+- [%s] %s -+-+-+-+-+-\n%!"
-    (string_of_fmt fmt)
-    (string_of_src src);
+  print (left (yellow "...") left_columns);
+  print_info (src, fmt);
   get src
   >>= fun xmlm_source ->
   Lwt.catch
@@ -94,31 +201,29 @@ let make_test (src, fmt, result) =
                           | errors ->
                             Lwt.return (W3CError (List.map Syndic.W3C.to_error errors)))
             | exn -> Lwt.return (AnotherError exn))
-  >>= function SyndicError (pos, err) ->
-               Printf.printf "Syndic: %s\n%!"
-                 (Syndic_error.to_string (Syndic_error.Error (pos, err)));
-
-               switch ();
-
-               Lwt.return ()
-             | W3CError errors ->
-               let add_error ch (pos, err) =
-                 Printf.fprintf ch "W3C: %s"
-                   (Syndic_error.Error (pos, err) |> Syndic_error.to_string)
-               in
-               Printf.printf "%a%!"
-                 (Printf.add_list ~sep:"\n" add_error) errors;
-               Lwt.return ()
-             | AnotherError exn ->
-               Printf.printf "Error: %s\n%!"
-                 (Printexc.to_string exn);
-
-               switch ();
-
-               Lwt.return ()
-             | Ok ->
-               Printf.printf "This document is valid!\n%!";
-               Lwt.return ()
+  >>= fun r -> match r with
+      | SyndicError (pos, err) ->
+        reset ();
+        print_result (src, fmt) (r, result);
+        newline ();
+        switch ();
+        Lwt.return ()
+      | W3CError errors ->
+        reset ();
+        print_result (src, fmt) (r, result);
+        newline ();
+        Lwt.return ()
+      | AnotherError exn ->
+        reset ();
+        print_result (src, fmt) (r, result);
+        newline ();
+        switch ();
+        Lwt.return ()
+      | Ok ->
+        reset ();
+        print_result (src, fmt) (r, result);
+        newline ();
+        Lwt.return ()
 
 let () =
   Lwt_unix.run (Lwt_list.map_s make_test tests >>= fun _ -> Lwt.return ());
