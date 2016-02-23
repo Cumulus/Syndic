dc5461936f0e9df45c7ecfd8d8e9d93cb716160e
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-02-23 04:36:28 -0800

    An example of relaxing parsing

--- bin/main.ml
+++ bin/main.ml
@@ -4,12 +4,22 @@ let () =
 
   Printexc.record_backtrace true;
 
-  try let lst, _ = W3C.parse (Xmlm.make_input (`Channel stdin)) in
-      List.iter
-        (fun (_, err) -> Printf.printf "E: %s\n%!" err)
-        (List.map Syndic.W3C.to_error lst)
+  try let rss2 =
+        Syndic.Rss2.relax
+          Rss2.Relax.({ channel with
+                        description = ignore;
+                        cloud = { cloud with
+                                  registerProcedure = ignore;
+                                  protocol          = ignore;
+                                  domain            = ignore;
+                                  port              = ignore;
+                                  path              = ignore;
+                                  uri =
+                                    (fun ~pos _ _ _ -> (None : Uri.t option)) } })
+          (Xmlm.make_input (`Channel stdin)) in
+      ()
   with
-    | W3C.Error.Error ((l, c), err) ->
+    | Rss2.Error.Error ((l, c), err) ->
       Printf.printf "[%d;%d]: %s\n%!"
         l c err
     | _ -> Printexc.print_backtrace stderr
