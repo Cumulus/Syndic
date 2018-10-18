fd40be6c78ebef3551db45505ccf9d43d0986238
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-10-18 16:43:38 +0200

    Fixed ambigious comments on Syndic_date.mli (and ocamlformat pass)

--- lib/syndic_date.mli
+++ lib/syndic_date.mli
@@ -1,7 +1,7 @@
 (** Minimal date module required by Syndic. *)
 
-type t = Ptime.t
 (** A date with time. *)
+type t = Ptime.t
 
 val epoch : t
 (** The POSIX time, i.e. Thursday, 1 January 1970 00:00:00 (UTC). *)
@@ -10,21 +10,32 @@ val compare : t -> t -> int
 (** Compare dates in increasing order. *)
 
 val max : t -> t -> t
-(** [max d1 d2] return the maximum (i.e. more recent) of the dates
-    [d1] and [d2]. *)
+(** [max d1 d2] return the maximum (i.e. more recent) of the dates [d1] and
+    [d2]. *)
+
 val min : t -> t -> t
-(** [min d1 d2] return the minimum (i.e. less recent) of the dates
-    [d1] and [d2]. *)
+(** [min d1 d2] return the minimum (i.e. less recent) of the dates [d1] and
+    [d2]. *)
 
 val of_rfc822 : string -> t
 val to_rfc822 : t -> string
-
 val of_rfc3339 : string -> t
 val to_rfc3339 : t -> string
 
 (** Month of the year. *)
 type month =
-  Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
+  | Jan
+  | Feb
+  | Mar
+  | Apr
+  | May
+  | Jun
+  | Jul
+  | Aug
+  | Sep
+  | Oct
+  | Nov
+  | Dec
 
 val string_of_month : month -> string
 (** Return the 3 letters identifying the month in English. *)
